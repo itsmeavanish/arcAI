79ba9dc1588d5ad430578d5e7136995d33d9203d
Anurag Upadhyay <anuragfx818@gmail.com>
2025-03-02 00:43:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/', // Change this if deploying to a subdirectory
-  build: {
-    rollupOptions: {
-      external: ['@vercel/analytics/react']
-    }
-  }
 })
