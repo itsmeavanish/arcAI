e6eb9248984abaadd9887c69a1226892c336581a
Anurag Upadhyay <anuragfx818@gmail.com>
2025-03-02 00:37:38 +0530

    Added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 // Adding Vercel Analytics
+import { Analytics } from "@vercel/analytics/react"
 // import { Analytics } from "@vercel/analytics/react"
 import Home from './Pages/Home';
 import About from './Pages/Aboutus';
